2cc34045668414a0500655823eadb2f730df5d7b
GERA <gerasimenko@kapremont.ru>
2018-05-14 13:49:23 +0300

    -

---
+++ 1._Главная_страница_личного_кабинета_5.sql
@@ -0,0 +1,112 @@
+prompt --application/set_environment
+set define off verify off feedback off
+whenever sqlerror exit sql.sqlcode rollback
+--------------------------------------------------------------------------------
+--
+-- ORACLE Application Express (APEX) export file
+--
+-- You should run the script connected to SQL*Plus as the Oracle user
+-- APEX_050100 or as the owner (parsing schema) of the application.
+--
+-- NOTE: Calls to apex_application_install override the defaults below.
+--
+--------------------------------------------------------------------------------
+begin
+wwv_flow_api.import_begin (
+ p_version_yyyy_mm_dd=>'2016.08.24'
+,p_release=>'5.1.4.00.08'
+,p_default_workspace_id=>11800973272798664
+,p_default_application_id=>103
+,p_default_owner=>'FCR'
+);
+end;
+/
+ 
+prompt APPLICATION 103 - LK
+--
+-- Application Export:
+--   Application:     103
+--   Name:            LK
+--   Date and Time:   13:49 Понедельник Май 14, 2018
+--   Exported By:     FCR
+--   Flashback:       0
+--   Export Type:     Page Export
+--   Version:         5.1.4.00.08
+--   Instance ID:     218235226215706
+--
+
+prompt --application/pages/delete_00001
+begin
+wwv_flow_api.remove_page (p_flow_id=>wwv_flow.g_flow_id, p_page_id=>1);
+end;
+/
+prompt --application/pages/page_00001
+begin
+wwv_flow_api.create_page(
+ p_id=>1
+,p_user_interface_id=>wwv_flow_api.id(11866208218552473)
+,p_name=>'Home'
+,p_page_mode=>'NORMAL'
+,p_step_title=>'Главная страница личного кабинета'
+,p_step_sub_title=>'Home'
+,p_step_sub_title_type=>'TEXT_WITH_SUBSTITUTIONS'
+,p_first_item=>'NO_FIRST_ITEM'
+,p_autocomplete_on_off=>'OFF'
+,p_group_id=>wwv_flow_api.id(12122140227731315)
+,p_step_template=>wwv_flow_api.id(12101358466597064)
+,p_page_template_options=>'#DEFAULT#'
+,p_overwrite_navigation_list=>'N'
+,p_page_is_public_y_n=>'N'
+,p_cache_mode=>'NOCACHE'
+,p_last_updated_by=>'GERA'
+,p_last_upd_yyyymmddhh24miss=>'20180514132405'
+);
+wwv_flow_api.create_page_plug(
+ p_id=>wwv_flow_api.id(22808257027743526)
+,p_plug_name=>'Суммарный баланс по всем моим помещениям'
+,p_region_template_options=>'#DEFAULT#:t-Region--scrollBody'
+,p_plug_template=>wwv_flow_api.id(11832695845552332)
+,p_plug_display_sequence=>10
+,p_include_in_reg_disp_sel_yn=>'Y'
+,p_plug_display_point=>'BODY'
+,p_plug_source=>'<H1><span style="color:&P1_COLOR.">&P1_BALANCE. руб.</span></H1>'
+,p_plug_query_options=>'DERIVED_REPORT_COLUMNS'
+,p_attribute_01=>'N'
+,p_attribute_02=>'HTML'
+);
+wwv_flow_api.create_page_item(
+ p_id=>wwv_flow_api.id(22809050426743534)
+,p_name=>'P1_BALANCE'
+,p_item_sequence=>10
+,p_item_plug_id=>wwv_flow_api.id(22808257027743526)
+,p_display_as=>'NATIVE_HIDDEN'
+,p_attribute_01=>'Y'
+);
+wwv_flow_api.create_page_item(
+ p_id=>wwv_flow_api.id(22809287478743536)
+,p_name=>'P1_COLOR'
+,p_item_sequence=>20
+,p_item_plug_id=>wwv_flow_api.id(22808257027743526)
+,p_display_as=>'NATIVE_HIDDEN'
+,p_attribute_01=>'Y'
+);
+wwv_flow_api.create_page_process(
+ p_id=>wwv_flow_api.id(22809157637743535)
+,p_process_sequence=>10
+,p_process_point=>'BEFORE_BOX_BODY'
+,p_process_type=>'NATIVE_PLSQL'
+,p_process_name=>'GET_USER_BALANCE'
+,p_process_sql_clob=>wwv_flow_string.join(wwv_flow_t_varchar2(
+'LK_INFO.get_user_balance(:APP_USER, :P1_BALANCE, :P1_COLOR);',
+''))
+,p_error_display_location=>'INLINE_IN_NOTIFICATION'
+);
+end;
+/
+begin
+wwv_flow_api.import_end(p_auto_install_sup_obj => nvl(wwv_flow_application_install.get_auto_install_sup_obj, false));
+commit;
+end;
+/
+set verify on feedback on define on
+prompt  ...done
